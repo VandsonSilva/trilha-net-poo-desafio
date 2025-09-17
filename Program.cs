22f9c0ccd7b1064122ce808f51dfffd72d497487
vandson silva <vandsonsilva@bokus.com.br>
2025-09-17 09:05:00 -0300

    Implementa classes Nokia e Iphone no desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("88888-8888", "iPhone 15", "987654321", 256);
+Smartphone nokia = new Nokia("99999 - 9999", "Nokia Tijolão", "123456789", 64);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("telegram");
+
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
